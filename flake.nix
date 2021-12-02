97131a89bb62e772c78e142d1eeffadb7ba8f1e1
mrkun <mrgutkun@gmail.com>
2021-12-02 18:00:01 +0300

    Move supercollider from jack; fix local Qt problem

--- flake.nix
+++ flake.nix
@@ -2,7 +2,12 @@
   outputs = { self, nixpkgs }: let system = "x86_64-linux"; in {
     devShell.${system} = with import nixpkgs { inherit system; }; mkShell {
       buildInputs = [
-        supercollider
+        (buildPackages.portaudio)
+        (supercollider.overrideAttrs ( oldAttrs: rec { 
+          cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DAUDIOAPI=portaudio" ] ; 
+          buildInputs = oldAttrs.buildInputs ++ [ portaudio ] ;
+        } ) )
+        # supercollider
         (haskellPackages.ghcWithPackages (a: [a.tidal]))
         (writeScriptBin "superdirt" ''
           sclang superdirt.scd
